cbff8134cbd2785a21ed184396c0c35d5ddc67af
Shahed1998 <shahedchowdhury120@gmail.com>
2022-06-16 21:18:12 +0600

    ...

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 const port = 3000;
 
-// Test Route
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side', app: 'Natours' });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: tours,
+  });
 });
 
 app.listen(port, () => {
