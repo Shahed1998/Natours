dc50a4bee13ee0f6ad33eb28bc96e64170e9fb03
Shahed1998 <shahedchowdhury120@gmail.com>
2022-09-14 02:57:51 +0600

    ...

--- src/controllers/errorController.js
+++ src/controllers/errorController.js
@@ -1,4 +1,21 @@
 /* eslint-disable no-param-reassign */
+const AppError = require('../utils/AppError');
+
+const handleCastErrorDB = (error) => {
+  const message = `Invalid ${error.path}: ${error.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateErrorDB = (error) => {
+  const message = `${error.keyValue.name}: already exist`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (error) => {
+  const errors = Object.values(error.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
 
 const saveErrDev = (err, res) => {
   res.status(err.statusCode).json({
@@ -11,6 +28,7 @@ const saveErrDev = (err, res) => {
 
 const saveErrProd = (err, res) => {
   // Operational error (trusted error)
+  // console.log(err, res);
   if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
@@ -19,7 +37,6 @@ const saveErrProd = (err, res) => {
   } else {
     // Programming or unknown error (untrusted)
     // 1. Log error
-    process.stderr.write('Error: ', err);
     // 2. Send generic message
     res.status(500).json({
       status: 'error',
@@ -31,11 +48,18 @@ const saveErrProd = (err, res) => {
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-
   if (process.env.NODE_ENV === 'development') {
     saveErrDev(err, res);
-  } else {
-    saveErrProd(err, res);
+  } else if (process.env.NODE_ENV === 'production ') {
+    let error = { ...err };
+    if (err.name === 'CastError') {
+      error = handleCastErrorDB(error);
+    } else if (err.code === 11000) {
+      error = handleDuplicateErrorDB(error);
+    } else if (err.name === 'ValidationError') {
+      error = handleValidationErrorDB(error);
+    }
+    saveErrProd(error, res);
   }
 
   next();
