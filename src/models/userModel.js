dcdb022ba1d57e8da3b26fa395fdc660450d3c40
Shahed1998 <shahedchowdhury120@gmail.com>
2022-09-17 02:20:32 +0600

    ...

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,5 +1,13 @@
+/* eslint-disable consistent-return */
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable object-shorthand */
+/* eslint-disable func-names */
+// eslint-disable prefer-arrow-callback
+
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
 // schema
 const userSchema = new mongoose.Schema({
   name: {
@@ -23,8 +31,26 @@ const userSchema = new mongoose.Schema({
   },
   passwordConfirm: {
     type: String,
+    validate: {
+      // This only works on save & save
+      validator: function (el) {
+        return el === this.password; // returns true / false
+      },
+      message: 'Passwords are not same', // error message
+    },
   },
 });
+
+userSchema.pre('save', async function (next) {
+  // run this function only if password is modified
+  if (!this.isModified('password')) return next();
+  // bcrypt hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+  // delete the password confirm field
+  this.passwordConfirm = undefined;
+  next();
+});
+
 // model
 const User = mongoose.model('User', userSchema);
 module.exports = User;
